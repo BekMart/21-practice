1aa69129f68e19d960e1594bca0d7eb9e5e2560b
Rebecca Martin <becki_213@hotmail.com>
2024-03-06 06:11:48 +0000

    Created global variables in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//Create global variables
+let opponentSum = 0;
+let playerSum = 0;
+//This will let us check what aces have been used
+let opponentAceCount = 0;
+let playerAceCount = 0;
+//Opponents hidden card and deck itself
+let hidden;
+let deck;
+//Allows player to draw a card if playerSum <=21
+let canHit = true;
