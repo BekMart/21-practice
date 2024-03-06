4b959054dc703430bae11c9953a6d591088abef6
Rebecca Martin <becki_213@hotmail.com>
2024-03-06 06:29:54 +0000

    Created deck

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
 //Create global variables
-let opponentSum = 0;
-let playerSum = 0;
+var opponentSum = 0;
+var playerSum = 0;
 //This will let us check what aces have been used
-let opponentAceCount = 0;
-let playerAceCount = 0;
+var opponentAceCount = 0;
+var playerAceCount = 0;
 //Opponents hidden card and deck itself
-let hidden;
-let deck;
+var hidden;
+var deck;
 //Allows player to draw a card if playerSum <=21
-let canHit = true;
+var canHit = true;
+
+window.onload = function() {
+    buildDeck();
+
+}
+
+function buildDeck() {
+    let values = ["a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"];
+    let types = ["c", "d", "h", "s"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let x = 0; x < values.length; x++) {
+            deck.push(values[x] + "-" + types[i]);
+        }
+    }
+    console.log(deck);
+}
