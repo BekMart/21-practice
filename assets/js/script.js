0eef444abef04d9a56c9d32ca238626951e92c99
Rebecca Martin <becki_213@hotmail.com>
2024-03-10 11:45:04 +0000

    Add event listener and function so page reloads when user clicks restart game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -261,30 +261,29 @@ function incrementPlayerScore() {
 
 
 
-
-
 // Get the modal
 var modal = document.getElementById("ruleModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("game-rules");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+// Created restart game function when user clicks restart game button
+document.getElementById("restart-game").addEventListener("click", restart);
+function restart() {
+    window.location.reload();
 }
